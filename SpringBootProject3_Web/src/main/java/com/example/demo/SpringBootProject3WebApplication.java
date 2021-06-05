045aa87be605682255fa4027332043de388e23fe
KiranDulam <kiransaichandra@gmail.com>
2021-06-05 12:46:40 -0400

    TestCommitGITTest1

---
+++ SpringBootProject3_Web/src/main/java/com/example/demo/SpringBootProject3WebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject3WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject3WebApplication.class, args);
+	}
+
+}
